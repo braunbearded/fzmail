87a19f9e0ed58b5bace2431484c043a66255a129
braunbearded <braunbearded1@gmail.com>
2020-12-06 17:45:57 +0100

    truncated folder as uniq key

--- profile-utils.sh
+++ profile-utils.sh
@@ -10,8 +10,11 @@ get_mails_by_profile() {
     #$1 = path to config
     #$2 = profile id
     #$3 = folder
-    mail_path="$(sed "$2!d" "$1" | awk -F "|" '{print $4}')/$3"
-    mlist "$mail_path"
+    mail_path="$(sed "$2!d" "$1" | awk -F "|" '{print $4}')"
+    mail_path_folder="$mail_path/$3"
+    [ ! -d "$mail_path_folder" ] && mail_path_folder="$(find "$mail_path" -type d | \
+        grep -Ev "(/cur$|/new$|/tmp$)" | grep -F "$3" | head -n 1)"
+    mlist "$mail_path_folder"
 }
 
 get_path_by_profile() {
